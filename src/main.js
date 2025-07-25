f020a4b0caf17de7f5d55711ddb078570e337234
danielhb90 <daniel.hincapieb@gmail.com>
2025-07-25 16:55:40 -0500

    agregar 5 imagenes en simultaneo

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 console.log('hola')
 
-const API_URL = 'https://api.thedogapi.com/v1/images/search';
+const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=5';
 
 
 
@@ -9,8 +9,17 @@ async function reload() {
     const res = await fetch(API_URL);
     const data = await res.json();
 
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+    const img1 = document.getElementById('img1');
+    const img2 = document.getElementById('img2');
+    const img3 = document.getElementById('img3');
+    const img4 = document.getElementById('img4');
+    const img5 = document.getElementById('img5');
+
+    img1.src = data[0].url;
+    img2.src = data[1].url;
+    img3.src = data[2].url;
+    img4.src = data[3].url;
+    img5.src = data[4].url;
     
 }
 
