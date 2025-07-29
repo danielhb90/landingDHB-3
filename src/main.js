c06226183505dd2ad630f07e55582a790e5c2bb8
danielhb90 <daniel.hincapieb@gmail.com>
2025-07-29 13:23:38 -0500

    cambios

--- src/main.js
+++ src/main.js
@@ -1,26 +1,64 @@
-console.log('hola')
 
-const API_URL = 'https://api.thedogapi.com/v1/images/search?limit=5';
+const API_URL_random = 'https://api.thedogapi.com/v1/images/search?limit=2&api_key=live_tMU3OaJc3maeoBa4oao50V3WAKlqFmiSqPu89546jJIC6I817aFEhlrvVO2SYSom';
+const API_URL_favorite = 'https://api.thedogapi.com/v1/favourites?limit=2';
 
+const spanError = document.getElementById('error');
 
+async function loadRandomDogs() {
 
-async function reload() {
-
-    const res = await fetch(API_URL);
+    const res = await fetch(API_URL_random);
     const data = await res.json();
+    console.log("RANDOM");
+    console.log(data)
+        
+    if (res.status === 200) {
+        spanError.innerText = 'Hubo un error: ' + res.status +' '+ data.message;
+    } else {
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');
 
-    const img1 = document.getElementById('img1');
-    const img2 = document.getElementById('img2');
-    const img3 = document.getElementById('img3');
-    const img4 = document.getElementById('img4');
-    const img5 = document.getElementById('img5');
-
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
-    img4.src = data[3].url;
-    img5.src = data[4].url;
-    
+        img1.src = data[0].url;
+        img2.src = data[1].url; 
+    }
+  
 }
 
-reload()
+// async function loadFavoriteDogs() {
+
+//     const res = await fetch(API_URL_favorite);
+//     const data = await res.json();
+//     console.log("FAVORITES");
+//     console.log(data)
+//     console.log(res.status)
+
+//     if (res.status !== 200) {
+//         spanError.innerHTML = 'Hubo un error: ' ;
+//     }
+
+// }
+
+// async function saveFavoriteDogs() {
+//     const res = await fetch(API_URL_favorite, {
+//         method: 'POST',
+//         header: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify({
+//             image_id: 12
+//         }),
+//     });
+//     const data = await res.json();
+//     console.log('SAVE AS FAVORITE')
+//     console.log(res)
+
+//     if (res.status !== 200) {
+//         spanError.innerHTML = 'Hubo un error: ' + res.status +' '+ data.message;
+//     }
+
+
+// }
+
+
+
+loadRandomDogs();
+
